de6de6f10e1e0a32298fc9a8ea06d1db999debb7
lyamano <437495449@qq.com>
2024-04-09 21:48:24 +0800

    接入openai，并返回正确结果

--- src/main/java/com/yupi/springbootinit/common/DeleteRequest.java
+++ src/main/java/com/yupi/springbootinit/common/DeleteRequest.java
@@ -1,13 +1,13 @@
 package com.yupi.springbootinit.common;
 
-import lombok.Data;
-
 import java.io.Serializable;
+import lombok.Data;
 
 /**
  * 删除请求
  *
- * @author Liu
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @Data
 public class DeleteRequest implements Serializable {
